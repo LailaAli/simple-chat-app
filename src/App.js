ba7f0b322a3e8a8fb6b4641c77079f6388245ea7
Laila Ali <laila@teambrightly.com>
2021-01-04 22:27:41 -0600

    Header: begin style

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import { Header } from './components/Header/Header';
+
+export const App = ()=> {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
