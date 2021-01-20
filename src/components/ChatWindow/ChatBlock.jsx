84c4afc73b0233709b44ab2415b525ccdf369e29
Laila Ali <laila@teambrightly.com>
2021-01-20 01:57:02 -0600

    Get channel details and messages, and style

--- src/components/ChatWindow/ChatBlock.jsx
+++ src/components/ChatWindow/ChatBlock.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import css from './ChatBlock.module.scss';
-// Needed to get id/category/extension of url
-import { useParams } from 'react-router-dom';
+
+// Icons
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+
+
 
 export const ChatBlock = props => {
   const { userImage, username, timestamp, message } = props;
 
-  const { channelsId } = useParams();
-
   return (
-    <div className={css.chatBlock}>
-      # {channelsId}
+    <div className={ css.chatBlock }>
+      <div className={ css.chatUserImage }>
+        { userImage ? <img src={userImage} alt="user" className={ css.userImage }/> : <AccountBoxIcon className={ css.userIcon } /> }
+      </div>
+      <div className={css.textColumn}>
+        <div className={css.textHeader}>
+          <span className={ css.userName }>{username }</span>
+          <span className={ css.timestamp }>{ timestamp}</span>
+        </div>
+        <div className={ css.message }>
+         {message}
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/ChatWindow/ChatWindow.jsx
+++ src/components/ChatWindow/ChatWindow.jsx
@@ -1,20 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import css from "./ChatWindow.module.scss";
+import classNames from "classnames";
 import { ChatBlock } from "./ChatBlock.jsx";
+// Get data from database
+import db from "../../firebase";
 // Needed to get id/category/extension of url
 import { useParams } from "react-router-dom";
+// Icons
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import PersonAddOutlinedIcon from "@material-ui/icons/PersonAddOutlined";
 
 export const ChatWindow = (props) => {
    const {} = props;
 
+   // Return an object of the channel rendered. Channel rendered is in the url.
    const { channelsId } = useParams();
+   const [channelsDetails, setChannelsDetails] = useState(null);
+   const [channelsMessages, setChannelsMessages] = useState([]);
+
+   // If there is a channel id in the url (useParams) then go to the database > collection(channel) > and get the data.
+   useEffect(() => {
+      if (channelsId) {
+         db.collection("channels")
+            .doc(channelsId)
+            .onSnapshot((snapshot) => setChannelsDetails(snapshot.data()));
+      }
+
+      // Get messages
+      db.collection("channels")
+         .doc(channelsId)
+         .collection("messages")
+         .orderBy("timestamp", "asc")
+         .onSnapshot(snapshot => setChannelsMessages(snapshot.docs.map((doc) => doc.data())));
+   }, [channelsId]); // <= run this useEffect everytime channelsId changes
+
+   console.log( channelsDetails );
+   console.log( channelsMessages );
 
    return (
       <div className={css.chatWindow}>
          <div className={css.header}>
-            <h3># {channelsId}</h3>
+            <div className={css.header_left}>
+               <h4 className={css.channelName}>
+                  #{channelsDetails?.name}
+                  <button className={css.button}>
+                     <StarBorderOutlinedIcon
+                        className={classNames(css.starIcon, css.icon)}
+                     />
+                  </button>
+               </h4>
+            </div>
+            <div className={css.header_right}>
+               <button className={css.button}>
+                  <PersonAddOutlinedIcon
+                     className={classNames(css.personIcon, css.icon)}
+                  />
+               </button>
+               <button className={css.button}>
+                  <InfoOutlinedIcon
+                     className={classNames(css.infoIcon, css.icon)}
+                  />
+               </button>
+            </div>
          </div>
-         <ChatBlock />
+
+        {channelsMessages?.map( msg => (
+           <ChatBlock
+              userImage={ msg.userImage }
+              username={ msg.user }
+              timestamp={ msg.timestamp.seconds }
+              message={ msg.message } />
+         ))}
       </div>
    );
 };
