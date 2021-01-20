8342fb3c6303df3a51d31f8da45fb15755e17ed2
Laila Ali <laila@teambrightly.com>
2021-01-19 21:52:09 -0600

    App: Include router and style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import './App.scss';
-import { Header } from './components/Header/Header';
-import { Sidebar } from './components/Sidebar/Sidebar';
+import css from "./App.module.scss";
+import { Header } from "./components/Header/Header.jsx";
+import { Sidebar } from "./components/Sidebar/Sidebar.jsx";
+import { ChatWindow } from "./components/ChatWindow/ChatWindow.jsx";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {ChatBlock} from './components/ChatWindow/ChatBlock'
 
-export const App = ()=> {
-  return (
-    <div className="App">
-      <Header/>
-      <Sidebar/>
-    </div>
-  );
-}
+export const App = () => {
+   return (
+      <div className={css.app}>
+         <Router>
+            <Header />
+            <div className={css.appBody}>
+               <Sidebar />
+               <Switch>
+                  <Route path="/channels/:channelsId">
+                     <ChatWindow/>
+                  </Route>
+                  <Route path="/">
+                     <ChatWindow />
+                  </Route>
+               </Switch>
+            </div>
+         </Router>
+      </div>
+   );
+};
 
 export default App;
