70b8f35924787c8d43162eddc4aa9ac05f5f8101
Laila Ali <laila@teambrightly.com>
2021-01-21 01:18:46 -0600

    Header: Update code to pull in user image and name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
-import css from './Header.module.scss';
+import React from "react";
+import css from "./Header.module.scss";
 // Icons
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
-import { Avatar } from '@material-ui/core';
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-import SearchIcon from '@material-ui/icons/Search';
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import { Avatar } from "@material-ui/core";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import SearchIcon from "@material-ui/icons/Search";
+import { useStateValue } from "../../StateProvider";
 
-export const Header = props => {
-  const {user } = props;
+export const Header = () => {
+   // Get logged in user info
+   const [{ user }] = useStateValue();
 
-  return (
-    <div className={ css.header }>
-      <div className={ css.header__left }> </div>
+   return (
+      <div className={css.header}>
+         <div className={css.header__left}> </div>
 
-      <div className={ css.header__search }>
-    
-        {/* <div className={css.header__searchInput}>
-          <SearchIcon></SearchIcon>
-          <input type="text" placeholder="Search Brightly" />
-        </div> */}
-          <button className={css.header__timeIcon}><AccessTimeIcon/></button>
-        <button className={css.header__searchButton}><SearchIcon></SearchIcon>
-          <span className={css.header__searchLabel}>Search Brightly</span>
-        </button>
+         <div className={css.header__search}>
+            <button className={css.header__timeIcon}>
+               <AccessTimeIcon />
+            </button>
+            <button className={css.header__searchButton}>
+               <SearchIcon></SearchIcon>
+               <span className={css.header__searchLabel}>Search Brightly</span>
+            </button>
 
-        <button className={css.helpIcon}><HelpOutlineIcon></HelpOutlineIcon></button>
+            <button className={css.helpIcon}>
+               <HelpOutlineIcon></HelpOutlineIcon>
+            </button>
+         </div>
+
+         <div className={css.header__right}>
+            <button className={css.header__avatarButton}>
+               <Avatar
+                  className={css.header__avatar}
+                  alt={user?.displayName}
+                  src={user?.photoURL}
+               ></Avatar>
+            </button>
+         </div>
       </div>
-
-      <div className={ css.header__right }>
-        
-        <button className={css.header__avatarButton}><Avatar className={css.header__avatar} alt={user?.displayName} src={user?.photoURL}></Avatar></button>
-      </div>
-    </div>
-  )
+   );
 };
