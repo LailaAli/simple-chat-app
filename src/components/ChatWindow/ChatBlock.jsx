f41c25e58d0809cbe69b51b6abfa4e930651b0d9
Laila Ali <laila@teambrightly.com>
2021-01-21 01:19:42 -0600

    Auto format

--- src/components/ChatWindow/ChatBlock.jsx
+++ src/components/ChatWindow/ChatBlock.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import css from './ChatBlock.module.scss';
+import React from "react";
+import css from "./ChatBlock.module.scss";
 
 // Icons
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
 
+export const ChatBlock = (props) => {
+   const { userImage, username, timestamp, message } = props;
 
-
-export const ChatBlock = props => {
-  const { userImage, username, timestamp, message } = props;
-
-  return (
-    <div className={ css.chatBlock }>
-      <div className={ css.chatUserImage }>
-        { userImage ? <img src={userImage} alt="user" className={ css.userImage }/> : <AccountBoxIcon className={ css.userIcon } /> }
+   return (
+      <div className={css.chatBlock}>
+         <div className={css.chatUserImage}>
+            {userImage ? (
+               <img src={userImage} alt="user" className={css.userImage} />
+            ) : (
+               <AccountBoxIcon className={css.userIcon} />
+            )}
+         </div>
+         <div className={css.textColumn}>
+            <div className={css.textHeader}>
+               <span className={css.userName}>{username}</span>
+               <span className={css.timestamp}>
+                  {new Date(timestamp?.toDate()).toUTCString()}
+               </span>
+            </div>
+            <div className={css.message}>{message}</div>
+         </div>
       </div>
-      <div className={css.textColumn}>
-        <div className={css.textHeader}>
-          <span className={ css.userName }>{username }</span>
-          <span className={ css.timestamp }>{new Date(timestamp?.toDate()).toUTCString()}</span>
-        </div>
-        <div className={ css.message }>
-         {message}
-        </div>
-      </div>
-    </div>
-  )
-}
+   );
+};
